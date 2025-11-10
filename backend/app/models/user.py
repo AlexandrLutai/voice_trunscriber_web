6a4b441bdd679082a6355d5b3d11eb634c2ce097
AlexandrLutay <alex896201090091@hmail.com>
2025-11-10 19:24:46 +0300

    Добавлено поле created_at

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,9 +1,10 @@
 from beanie import Document
-from datetime import datetime
+from datetime import datetime, timezone
 
 class User(Document):
     username:str
     password:str
+    created_at:datetime = datetime.now(timezone.utc)
     class Settings:
         name = "users"
         validate_on_save = True
